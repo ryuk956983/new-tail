1d5c99ab132e077e6648994efb44022d67a0b250
ryuk956983 <106580255+ryuk956983@users.noreply.github.com>
2024-05-25 03:46:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from './Components/Footer'
 
 const App = () => {
   return (
-    <div className='bg-amber-100 lg:h-screen lg:py-12 lg:px-28 flex flex-col justify-between px-10 py-5 w-full gap-10'>
+    <div className='bg-amber-100 lg:h-screen lg:py-12 lg:px-28 flex flex-col justify-between px-10 py-5 w-screen gap-10'>
       <Header/>
       <Body/>
       <Footer/>
